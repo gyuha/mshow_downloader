e5c14f2fbb8f87e16bb13483c82705cd61f6fe06
gyuha <nicegyuha@gmail.com>
2019-03-05 14:27:52 +0900

    icon 및 버전 정보 추가

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 CALL .\venv\Scripts\activate.bat
-pyinstaller -y -F "mangashowme.py"
+pyinstaller -y -F "mangashowme.py" --version-file=file_version_info.txt -i icon.ico
 copy chromedriver.exe dist
 copy config.ini dist
 CALL .\venv\Scripts\deactivate.bat
