45d4f9512e568da98894d5a137afdceb4138f02f
gyuha <nicegyuha@gmail.com>
2020-10-11 02:54:08 +0900

    update

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 CALL .\venv\Scripts\activate.bat
-pyinstaller -y  --log-level=DEBUG --onefile --clean -F "manatoki.py" --version-file=file_version_info.txt -i icon.ico
+pyinstaller --windowed --clean -F "manatoki.py" --version-file=file_version_info.txt -i icon.ico
 copy phantomjs.exe dist
 copy chromedriver.exe dist
 copy manatoki.ini dist
